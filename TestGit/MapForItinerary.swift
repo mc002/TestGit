59f10839c561f78602ce076f862f3bb29d665294
Martin Cermák <mc002@atlas.cz>
2018-08-24 18:34:49 +0200

    map feature

---
+++ TestGit/MapForItinerary.swift
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.swift
+//  TestGit
+//
+//  Created by Martin Čermák on 24/08/2018.
+//  Copyright © 2018 Martin Čermák. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+    
+    
+}
