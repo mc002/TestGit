43ccc72be825679848ffab4070ffa6876729d5aa
Martin Cermák <mc002@atlas.cz>
2018-08-24 18:10:05 +0200

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Martin Čermák on 24/08/2018.
+//  Copyright © 2018 Martin Čermák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
